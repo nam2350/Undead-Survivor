68272c2ebcf198651539311df72e6096e4e80f73
Nam Jae Hyun <nam2350@gmail.com>
2023-01-09 17:38:05 +0900

    Level Design

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;    
+
     public Player player;
     public PoolManager pool;
 
-    private void Awake()
+    void Awake()
     {
         instance = this;
     }
 
+    void Update() {
+        gameTime += Time.deltaTime;
+
+        if(gameTime > maxGameTime) {
+           gameTime = maxGameTime;
+        }        
+    }
 }
